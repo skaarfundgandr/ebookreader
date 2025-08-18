ab76b7eabcd473a39f85d9e0fcfbaf230e6ad366
jm-eospc <mac2x.manos@gmail.com>
2025-08-18 11:53:56 +0800

    CHORE: Initialize directory for commands

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use ebookreader_lib::commands::sample;
+
 fn main() {
-    ebookreader_lib::run()
+    sample::run();
 }
