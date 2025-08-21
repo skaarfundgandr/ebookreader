61ce174f9eb669d7b55f4efcc337882239dd747c
jm-nitrolinux <mac2x.manos@gmail.com>
2025-08-21 11:32:49 +0800

    FEAT: Add simple read operations on configuration table and improve error handling in get_all_configurations

--- src-tauri/src/data/repos/configuration_repo.rs
+++ src-tauri/src/data/repos/configuration_repo.rs
@@ -1,23 +1,51 @@
 use diesel::prelude::*;
+use diesel::result::*;
 use diesel_async::RunQueryDsl;
 
 use crate::data::database::*;
 use crate::data::models::configuration::Configuration;
 
-pub async fn get_all_configuration() -> Result<Option<Vec<Configuration>>, diesel::result::Error> {
+pub async fn get_all_configurations() -> Result<Option<Vec<Configuration>>, Error> {
     use crate::data::models::schema::configuration::dsl::*;
 
-    let mut conn = match connect_from_pool().await {
-        Ok(value) => value,
-        Err(_) => panic!("Failed to connect from pool"),
-    };
+    let mut conn = connect_from_pool()
+        .await
+        .map_err(|e| {
+            Error::DatabaseError (
+                DatabaseErrorKind::UnableToSendCommand,
+                Box::new(e.to_string())
+            )
+        })?;
 
     return match configuration
         .select(Configuration::as_select())
         .load(&mut conn)
         .await {
             Ok(value) => Ok(Some(value)),
-            Err(diesel::result::Error::NotFound) => Ok(None),
+            Err(Error::NotFound) => Ok(None),
             Err(e) => Err(e),
         };
+}
+
+pub async fn get_book_path() -> Result<Option<String>, Error> {
+    use crate::data::models::schema::configuration::dsl::*;
+    
+    let mut conn = connect_from_pool()
+        .await
+        .map_err(|e| {
+            Error::DatabaseError(
+                DatabaseErrorKind::UnableToSendCommand,
+                Box::new(e.to_string())
+            )
+        })?;
+
+    return match configuration
+        .select(book_path)
+        .first::<Option<String>>(&mut conn)
+        .await
+        .optional() {
+            Ok(Some(value)) => Ok(value),
+            Ok(None) => Ok(None),
+            Err(e) => Err(e)
+        }
 }
