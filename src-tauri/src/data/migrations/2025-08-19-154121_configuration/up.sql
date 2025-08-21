ce952f8bca2319c5aefdf810e18b235844b17095
jm-nitrolinux <mac2x.manos@gmail.com>
2025-08-21 11:36:54 +0800

    FEAT: Slight improvement on db table and ignored db file

--- src-tauri/src/data/migrations/2025-08-19-154121_configuration/up.sql
+++ src-tauri/src/data/migrations/2025-08-19-154121_configuration/up.sql
@@ -1,5 +1,5 @@
 -- Your SQL goes here
 CREATE TABLE `configuration` (
     `configuration_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `book_path` VARCHAR
+    `book_path` VARCHAR DEFAULT NULL
 )
