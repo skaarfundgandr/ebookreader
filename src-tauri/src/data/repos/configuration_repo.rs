0aa6bc019c55bf064e7e49cd221377cc35a35eec
jm-nitrolinux <mac2x.manos@gmail.com>
2025-09-19 00:46:13 +0800

    FEAT: Add book path logic on configuration

--- src-tauri/src/data/repos/configuration_repo.rs
+++ src-tauri/src/data/repos/configuration_repo.rs
@@ -1,9 +1,12 @@
 use diesel::prelude::*;
 use diesel::result::*;
+use diesel_async::scoped_futures::ScopedFutureExt;
+use diesel_async::AsyncConnection;
 use diesel_async::RunQueryDsl;
 
 use crate::data::database::*;
 use crate::data::models::configuration::Configuration;
+use crate::data::models::configuration::ConfigurationForm;
 
 pub async fn get_all_configurations() -> Result<Option<Vec<Configuration>>, Error> {
     use crate::data::models::schema::configuration::dsl::*;
@@ -48,4 +51,39 @@ pub async fn get_book_path() -> Result<Option<String>, Error> {
             Ok(None) => Ok(None),
             Err(e) => Err(e)
         }
+}
+
+pub async fn set_book_path(new_path: &str) -> Result<(), Error> {
+    use crate::data::models::schema::configuration::dsl::*;
+
+    let new_config: ConfigurationForm<'_> = ConfigurationForm {
+        book_path: Some(new_path),
+    };
+    
+    let pool_conn = connect_from_pool().await;
+
+    let mut conn = match pool_conn {
+        Ok(value) => value,
+        Err(e) => return Err(Error::DatabaseError(
+            DatabaseErrorKind::UnableToSendCommand,
+            Box::new(e.to_string())
+        )),
+    };
+
+    return match conn.transaction(|connection|
+        async move {
+            diesel::insert_into(configuration)
+                .values(&new_config)
+                .on_conflict(configuration_id)
+                .do_update()
+                .set(&new_config)
+                .execute(connection)
+                .await?;
+
+            Ok(())
+        }.scope_boxed()
+    ).await {
+        Ok(value) => Ok(value),
+        Err(e) => Err(e)
+    };
 }
