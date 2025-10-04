02e9d7ea95a08e71014617de18387923797de4e7
jm-eospc <mac2x.manos@gmail.com>
2025-10-04 22:22:37 +0800

    REFAC: Rename id to book_id in books table and make file_path nullable

--- src-tauri/src/data/migrations/2025-10-01-021741-0000_books/up.sql
+++ src-tauri/src/data/migrations/2025-10-01-021741-0000_books/up.sql
@@ -1,12 +1,12 @@
 -- Your SQL goes here
 CREATE TABLE `books` (
-    `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `book_id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     `title` TEXT NOT NULL,
     `published_date` TEXT,
     `publisher_id` INTEGER,
     `isbn` TEXT,
     `file_type` TEXT,
-    `file_path` TEXT NOT NULL,
+    `file_path` TEXT,
     `added_at` TEXT DEFAULT (datetime('now')),
     FOREIGN KEY (`publisher_id`) REFERENCES `publishers`(`publisher_id`) ON DELETE SET NULL
 );
