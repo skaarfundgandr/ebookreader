7cbfc63e0cf8fb36d4523930cd1ab43cf5b061c6
jm-eospc <mac2x.manos@gmail.com>
2025-10-04 12:55:04 +0800

    FEAT: Create migrations using diesel

--- src-tauri/src/data/models/schema.rs
+++ src-tauri/src/data/models/schema.rs
@@ -1,9 +1,45 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    authors (author_id) {
+        author_id -> Integer,
+        name -> Text,
+    }
+}
+
+diesel::table! {
+    books (id) {
+        id -> Integer,
+        title -> Text,
+        author_id -> Nullable<Integer>,
+        published_date -> Nullable<Text>,
+        publisher_id -> Nullable<Integer>,
+        isbn -> Nullable<Text>,
+        file_type -> Nullable<Text>,
+        file_path -> Text,
+        added_at -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
     configuration (configuration_id) {
         configuration_id -> Integer,
         book_path -> Nullable<Text>,
+    }
+}
+
+diesel::table! {
+    publishers (publisher_id) {
+        publisher_id -> Integer,
+        name -> Text,
+    }
+}
+
+diesel::table! {
+    user_library (user_id, book_id) {
+        user_id -> Integer,
+        book_id -> Integer,
+        added_at -> Nullable<Text>,
     }
 }
 
@@ -13,8 +49,20 @@ diesel::table! {
         username -> Text,
         email -> Text,
         password_hash -> Text,
-        created_at -> Text,
+        created_at -> Nullable<Text>,
     }
 }
 
-diesel::allow_tables_to_appear_in_same_query!(configuration, users,);
+diesel::joinable!(books -> authors (author_id));
+diesel::joinable!(books -> publishers (publisher_id));
+diesel::joinable!(user_library -> books (book_id));
+diesel::joinable!(user_library -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    authors,
+    books,
+    configuration,
+    publishers,
+    user_library,
+    users,
+);
