aab3621e7d0e24bbca513115a4c1855e9ca5be01
jm-eospc <mac2x.manos@gmail.com>
2025-10-05 10:47:06 +0800

    FIX: Remove ON DELETE CASCADE from foreign key constraints in user_library table because it is interfering with tests

--- src-tauri/src/data/migrations/2025-10-01-020316-0000_user_library/up.sql
+++ src-tauri/src/data/migrations/2025-10-01-020316-0000_user_library/up.sql
@@ -4,6 +4,6 @@ CREATE TABLE `user_library` (
     `book_id` INTEGER NOT NULL,
     `added_at` TEXT DEFAULT (datetime('now')),
     PRIMARY KEY (`user_id`, `book_id`),
-    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE,
-    FOREIGN KEY (`book_id`) REFERENCES `books`(`id`) ON DELETE CASCADE
+    FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`),
+    FOREIGN KEY (`book_id`) REFERENCES `books`(`id`)
 );
