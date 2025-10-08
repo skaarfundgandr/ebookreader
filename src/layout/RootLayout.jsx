98096395b45aebcb2ba5da5aa13082c6c75613c4
jm-eospc <mac2x.manos@gmail.com>
2025-10-08 13:47:35 +0800

    FIX: Correct import path for Sidebar component in RootLayout

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 
 export default function RootLayout() {
