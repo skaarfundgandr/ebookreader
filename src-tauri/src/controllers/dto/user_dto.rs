ad47270f3c5ef0b3472f913228059798e4918bb3
jm-nitrolinux <mac2x.manos@gmail.com>
2025-11-12 22:04:53 +0800

    REFAC: Move mapping logic to different file

--- src-tauri/src/controllers/dto/user_dto.rs
+++ src-tauri/src/controllers/dto/user_dto.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-use crate::data::models::users::Users;
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UserDTO {
     pub username: String,
@@ -13,26 +11,6 @@ pub struct UserDTO {
 pub struct NewUserDTO {
     pub username: String,
     pub email: String,
-    pub password_hash: String,
+    pub password: String,
     pub created_at: Option<String>,
-}
-
-impl From<NewUserDTO> for UserDTO {
-    fn from(user: NewUserDTO) -> Self {
-        UserDTO {
-            username: user.username.to_string(),
-            email: user.email.to_string(),
-            created_at: user.created_at.map(|s| s.to_string()),
-        }
-    }
-}
-
-impl From<Users> for UserDTO {
-    fn from(user: Users) -> Self {
-        UserDTO {
-            username: user.username,
-            email: user.email,
-            created_at: user.created_at,
-        }
-    }
 }
--- src-tauri/src/utils/mappers.rs
+++ src-tauri/src/utils/mappers.rs
@@ -1,1 +1,23 @@
 //TODO: Move mappers (From and Into) here
+use crate::controllers::dto::user_dto::{NewUserDTO, UserDTO};
+use crate::data::models::users::Users;
+
+impl From<NewUserDTO> for UserDTO {
+    fn from(user: NewUserDTO) -> Self {
+        UserDTO {
+            username: user.username.to_string(),
+            email: user.email.to_string(),
+            created_at: user.created_at.map(|s| s.to_string()),
+        }
+    }
+}
+
+impl From<Users> for UserDTO {
+    fn from(user: Users) -> Self {
+        UserDTO {
+            username: user.username,
+            email: user.email,
+            created_at: user.created_at,
+        }
+    }
+}
